b8503518d78dadb63c6a5352b10f8ab288d682c5
Miguel Albernaz <albernazmiguel@gmail.com>
2025-02-05 06:35:36 -0300

    more refactoring

--- src/node.rs
+++ src/node.rs
@@ -24,208 +24,170 @@ use crate::{
     },
 };
 
-pub struct LightingPass<'w> {
+fn run_lighting_pass<'w>(
     world: &'w World,
-}
+    render_context: &mut RenderContext<'w>,
+    camera: &ExtractedCamera,
+    input: &CachedTexture,
+    output: &CachedTexture,
+    view_entity: &Entity,
+    view_uniform_offset: u32,
+    settings_uniform_offset: u32,
+) {
+    let prepass_pipelines = world.resource::<Lighting2dPrepassPipelines>();
+    let pipeline_cache = world.resource::<PipelineCache>();
 
-impl<'w> LightingPass<'w> {
-    pub fn new(world: &'w World) -> Self {
-        Self { world }
+    let (Some(pipeline), Some(view_uniforms), Some(lighting_settings_uniforms), Some(point_lights)) = (
+        pipeline_cache.get_render_pipeline(prepass_pipelines.lighting_pipeline),
+        world.resource::<ViewUniforms>().uniforms.binding(),
+        world
+            .resource::<ComponentUniforms<ExtractedLighting2dSettings>>()
+            .binding(),
+        world
+            .resource::<Lighing2dViewArrayBuffer<ExtractedPointLight2d>>()
+            .get(view_entity),
+    ) else {
+        return;
+    };
+
+    let (Some(point_lights), Some(point_lights_count)) =
+        (point_lights.data.binding(), point_lights.count.binding())
+    else {
+        return;
+    };
+
+    let sampler = render_context
+        .render_device()
+        .create_sampler(&SamplerDescriptor::default());
+
+    let bind_group = render_context.render_device().create_bind_group(
+        "lighting2d_bind_group",
+        &prepass_pipelines.lighting_layout,
+        &BindGroupEntries::sequential((
+            view_uniforms,
+            lighting_settings_uniforms,
+            point_lights,
+            point_lights_count,
+            &input.default_view,
+            &sampler,
+        )),
+    );
+
+    let mut pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
+        label: Some("lighting_pass"),
+        color_attachments: &[Some(RenderPassColorAttachment {
+            view: &output.default_view,
+            resolve_target: None,
+            ops: Operations::default(),
+        })],
+        ..default()
+    });
+
+    if let Some(viewport) = camera.viewport.as_ref() {
+        pass.set_camera_viewport(viewport);
     }
 
-    pub fn execute(
-        &mut self,
-        ctx: &mut RenderContext<'_>,
-        camera: &ExtractedCamera,
-        input: &CachedTexture,
-        output: &CachedTexture,
-        view_entity: &Entity,
-        view_uniform_offset: u32,
-        settings_uniform_offset: u32,
-    ) {
-        let world = self.world;
-        let prepass_pipelines = world.resource::<Lighting2dPrepassPipelines>();
-        let pipeline_cache = world.resource::<PipelineCache>();
-
-        let (
-            Some(pipeline),
-            Some(view_uniforms),
-            Some(lighting_settings_uniforms),
-            Some(point_lights),
-        ) = (
-            pipeline_cache.get_render_pipeline(prepass_pipelines.lighting_pipeline),
-            world.resource::<ViewUniforms>().uniforms.binding(),
-            world
-                .resource::<ComponentUniforms<ExtractedLighting2dSettings>>()
-                .binding(),
-            world
-                .resource::<Lighing2dViewArrayBuffer<ExtractedPointLight2d>>()
-                .get(view_entity),
-        )
-        else {
-            return;
-        };
-
-        let (Some(point_lights), Some(point_lights_count)) =
-            (point_lights.data.binding(), point_lights.count.binding())
-        else {
-            return;
-        };
-
-        let sampler = ctx
-            .render_device()
-            .create_sampler(&SamplerDescriptor::default());
-
-        let bind_group = ctx.render_device().create_bind_group(
-            "lighting2d_bind_group",
-            &prepass_pipelines.lighting_layout,
-            &BindGroupEntries::sequential((
-                view_uniforms,
-                lighting_settings_uniforms,
-                point_lights,
-                point_lights_count,
-                &input.default_view,
-                &sampler,
-            )),
-        );
-
-        let mut pass = ctx.begin_tracked_render_pass(RenderPassDescriptor {
-            label: Some("lighting_pass"),
-            color_attachments: &[Some(RenderPassColorAttachment {
-                view: &output.default_view,
-                resolve_target: None,
-                ops: Operations::default(),
-            })],
-            ..default()
-        });
-
-        if let Some(viewport) = camera.viewport.as_ref() {
-            pass.set_camera_viewport(viewport);
-        }
-
-        pass.set_render_pipeline(pipeline);
-        pass.set_bind_group(
-            0,
-            &bind_group,
-            &[view_uniform_offset, settings_uniform_offset],
-        );
-        pass.draw(0..3, 0..1);
-    }
+    pass.set_render_pipeline(pipeline);
+    pass.set_bind_group(
+        0,
+        &bind_group,
+        &[view_uniform_offset, settings_uniform_offset],
+    );
+    pass.draw(0..3, 0..1);
 }
 
-pub struct BlurPass<'w> {
+fn run_blur_pass<'w>(
     world: &'w World,
+    render_context: &mut RenderContext<'w>,
+    input: &CachedTexture,
+    output: &CachedTexture,
+    view_uniform_offset: u32,
+    settings_uniform_offset: u32,
+) {
+    let prepass_pipelines = world.resource::<Lighting2dPrepassPipelines>();
+    let pipeline_cache = world.resource::<PipelineCache>();
+
+    let (Some(pipeline), Some(view_uniforms), Some(lighting_settings_uniforms)) = (
+        pipeline_cache.get_render_pipeline(prepass_pipelines.blur_pipeline),
+        world.resource::<ViewUniforms>().uniforms.binding(),
+        world
+            .resource::<ComponentUniforms<ExtractedLighting2dSettings>>()
+            .binding(),
+    ) else {
+        return;
+    };
+
+    let sampler = render_context
+        .render_device()
+        .create_sampler(&SamplerDescriptor::default());
+
+    let bind_group = render_context.render_device().create_bind_group(
+        "blur_bind_group",
+        &prepass_pipelines.blur_layout,
+        &BindGroupEntries::sequential((
+            view_uniforms,
+            lighting_settings_uniforms,
+            &input.default_view,
+            &sampler,
+        )),
+    );
+
+    let mut pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
+        label: Some("blur_pass"),
+        color_attachments: &[Some(RenderPassColorAttachment {
+            view: &output.default_view,
+            resolve_target: None,
+            ops: Operations::default(),
+        })],
+        ..default()
+    });
+
+    pass.set_render_pipeline(pipeline);
+    pass.set_bind_group(
+        0,
+        &bind_group,
+        &[view_uniform_offset, settings_uniform_offset],
+    );
+    pass.draw(0..3, 0..1);
 }
 
-impl<'w> BlurPass<'w> {
-    pub fn new(world: &'w World) -> Self {
-        Self { world }
-    }
-
-    pub fn execute(
-        &mut self,
-        ctx: &mut RenderContext<'_>,
-        input: &CachedTexture,
-        output: &CachedTexture,
-        view_uniform_offset: u32,
-        settings_uniform_offset: u32,
-    ) {
-        let world = self.world;
-        let prepass_pipelines = world.resource::<Lighting2dPrepassPipelines>();
-        let pipeline_cache = world.resource::<PipelineCache>();
-
-        let (Some(pipeline), Some(view_uniforms), Some(lighting_settings_uniforms)) = (
-            pipeline_cache.get_render_pipeline(prepass_pipelines.blur_pipeline),
-            world.resource::<ViewUniforms>().uniforms.binding(),
-            world
-                .resource::<ComponentUniforms<ExtractedLighting2dSettings>>()
-                .binding(),
-        ) else {
-            return;
-        };
-
-        let sampler = ctx
-            .render_device()
-            .create_sampler(&SamplerDescriptor::default());
-
-        let bind_group = ctx.render_device().create_bind_group(
-            "blur_bind_group",
-            &prepass_pipelines.blur_layout,
-            &BindGroupEntries::sequential((
-                view_uniforms,
-                lighting_settings_uniforms,
-                &input.default_view,
-                &sampler,
-            )),
-        );
-
-        let mut pass = ctx.begin_tracked_render_pass(RenderPassDescriptor {
-            label: Some("blur_pass"),
-            color_attachments: &[Some(RenderPassColorAttachment {
-                view: &output.default_view,
-                resolve_target: None,
-                ops: Operations::default(),
-            })],
-            ..default()
-        });
-
-        pass.set_render_pipeline(pipeline);
-        pass.set_bind_group(
-            0,
-            &bind_group,
-            &[view_uniform_offset, settings_uniform_offset],
-        );
-        pass.draw(0..3, 0..1);
-    }
-}
-
-pub struct CompositePass<'w> {
+pub fn run_composite_pass<'w>(
     world: &'w World,
-}
+    render_context: &mut RenderContext<'w>,
+    input: &CachedTexture,
+    view_target: &ViewTarget,
+    pipeline_id: CachedRenderPipelineId,
+) {
+    let pipeline_cache = world.resource::<PipelineCache>();
 
-impl<'w> CompositePass<'w> {
-    pub fn new(world: &'w World) -> Self {
-        Self { world }
-    }
+    let Some(pipeline) = pipeline_cache.get_render_pipeline(pipeline_id) else {
+        return;
+    };
 
-    pub fn execute(
-        &mut self,
-        ctx: &mut RenderContext<'_>,
-        input: &CachedTexture,
-        view_target: &ViewTarget,
-        pipeline_id: CachedRenderPipelineId,
-    ) {
-        let pipeline_cache = self.world.resource::<PipelineCache>();
+    let post_process = view_target.post_process_write();
+    let sampler = render_context
+        .render_device()
+        .create_sampler(&SamplerDescriptor::default());
 
-        let Some(pipeline) = pipeline_cache.get_render_pipeline(pipeline_id) else {
-            return;
-        };
+    let bind_group = render_context.render_device().create_bind_group(
+        "composite_bind_group",
+        &world.resource::<Lighting2dCompositePipeline>().layout,
+        &BindGroupEntries::sequential((post_process.source, &input.default_view, &sampler)),
+    );
 
-        let post_process = view_target.post_process_write();
-        let sampler = ctx
-            .render_device()
-            .create_sampler(&SamplerDescriptor::default());
+    let mut pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
+        label: Some("composite_pass"),
+        color_attachments: &[Some(RenderPassColorAttachment {
+            view: post_process.destination,
+            resolve_target: None,
+            ops: Operations::default(),
+        })],
+        ..default()
+    });
 
-        let bind_group = ctx.render_device().create_bind_group(
-            "composite_bind_group",
-            &self.world.resource::<Lighting2dCompositePipeline>().layout,
-            &BindGroupEntries::sequential((post_process.source, &input.default_view, &sampler)),
-        );
-
-        let mut pass = ctx.begin_tracked_render_pass(RenderPassDescriptor {
-            label: Some("composite_pass"),
-            color_attachments: &[Some(RenderPassColorAttachment {
-                view: post_process.destination,
-                resolve_target: None,
-                ops: Operations::default(),
-            })],
-            ..default()
-        });
-
-        pass.set_render_pipeline(pipeline);
-        pass.set_bind_group(0, &bind_group, &[]);
-        pass.draw(0..3, 0..1);
-    }
+    pass.set_render_pipeline(pipeline);
+    pass.set_bind_group(0, &bind_group, &[]);
+    pass.draw(0..3, 0..1);
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
@@ -247,7 +209,7 @@ impl ViewNode for LightingNode {
     fn run<'w>(
         &self,
         graph: &mut RenderGraphContext,
-        ctx: &mut RenderContext<'w>,
+        render_context: &mut RenderContext<'w>,
         (
             view_target,
             camera,
@@ -261,10 +223,9 @@ impl ViewNode for LightingNode {
     ) -> Result<(), NodeRunError> {
         let mut voronoi_texture = voronoi_texture.clone();
 
-        // Lighting
-        let mut lighting_pass = LightingPass::new(world);
-        lighting_pass.execute(
-            ctx,
+        run_lighting_pass(
+            world,
+            render_context,
             camera,
             voronoi_texture.input(),
             voronoi_texture.output(),
@@ -274,11 +235,10 @@ impl ViewNode for LightingNode {
         );
         voronoi_texture.flip();
 
-        // Blur
         if lighting_settings.blur > 0.0 {
-            let mut blur_pass = BlurPass::new(world);
-            blur_pass.execute(
-                ctx,
+            run_blur_pass(
+                world,
+                render_context,
                 voronoi_texture.input(),
                 voronoi_texture.output(),
                 view_uniform_offset.offset,
@@ -287,10 +247,9 @@ impl ViewNode for LightingNode {
             voronoi_texture.flip();
         }
 
-        // Post Process
-        let mut composite_pass = CompositePass::new(world);
-        composite_pass.execute(
-            ctx,
+        run_composite_pass(
+            world,
+            render_context,
             voronoi_texture.input(),
             view_target,
             composite_pipeline_id.0,
